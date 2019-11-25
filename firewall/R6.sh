addf22269cd61e3d68ea98a59a06d2996ded01d3
vagrant <vagrant@group2>
2019-11-25 13:00:26 +0000

    last commit

--- firewall/R6.sh
+++ firewall/R6.sh
@@ -3,7 +3,7 @@ ip6tables -F FORWARD
 ip6tables -F INPUT
 ip6tables -F OUTPUT
 
-#These are the default rules (Exery packet not known by the rules will be dropped)
+#These are the default rules (Exery packet not known by the rules will be droped)
 ip6tables -P FORWARD DROP
 ip6tables -P INPUT DROP
 ip6tables -P OUTPUT DROP
@@ -12,11 +12,16 @@ ip6tables -P OUTPUT DROP
 ip6tables -A INPUT -p 89 -j ACCEPT
 ip6tables -A OUTPUT -p 89 -j ACCEPT
 
-#This will block all OSPF packets coming from/going to outside of the network
-ip6tables -A FORWARD -i ebG1 -p 89 -j DROP
-ip6tables -A FORWARD -o ebG1 -p 89 -j DROP
-ip6tables -A INPUT -i ebG1 -p 89 -j DROP
-ip6tables -A OUTPUT -o ebG1 -p 89 -j DROP
+#This will block all OSPF packets coming from/going to the outside of the network
+ip6tables -A FORWARD -i eb1 -p 89 -j DROP
+ip6tables -A FORWARD -o eb1 -p 89 -j DROP
+ip6tables -A INPUT -i eb1 -p 89 -j DROP
+ip6tables -A OUTPUT -o eb1 -p 89 -j DROP
+ip6tables -A FORWARD -i ebG6 -p 89 -j DROP
+ip6tables -A FORWARD -o ebG6 -p 89 -j DROP
+ip6tables -A INPUT -i ebG6 -p 89 -j DROP
+ip6tables -A OUTPUT -o ebG6 -p 89 -j DROP
+
 
 #This will allow all BGP packets
 ip6tables -A INPUT -p 179 -j ACCEPT
--- verifEtatTopo/main.sh
+++ verifEtatTopo/main.sh
@@ -2,9 +2,9 @@
 
 cd ..
 
-#sudo ./cleanup.sh > /dev/null 2>&1
+sudo ./cleanup.sh > /dev/null 2>&1
 
-#sudo ./create_network.sh myNetwork > /dev/null 2>&1
+sudo ./create_network.sh myNetwork > /dev/null 2>&1
 
 cd verifEtatTopo
 
@@ -12,10 +12,10 @@ rm resultPING &> /dev/null
 rm resultTraceRoute &> /dev/null
 rm resultBGP &> /dev/null
 
-#sudo ./testPINGBase.sh >> resultPING
-#sudo ./testPINGOSPF.sh >> resultPING
-#sudo ./testPINGLoopback.sh >> resultPING
-#python readPING.py
+sudo ./testPINGBase.sh >> resultPING
+sudo ./testPINGOSPF.sh >> resultPING
+sudo ./testPINGLoopback.sh >> resultPING
+python readPING.py
 
 sudo ./testTraceRoute.sh > resultTraceRoute
 python readTraceRoute.py
